ca3b608657c0a13a41776f94d1e27f48a330112c
Aeva Palecek <aeva.ntsc@gmail.com>
2019-02-25 00:30:56 -0600

    more csg :O

--- regen_shapes.scm
+++ regen_shapes.scm
@@ -20,13 +20,37 @@
 
 ;; CSG tree to be rendered:
 (define csg-tree
-  '(minus
-    (sphere 200)
-    (translate (-50 -50 100) (sphere 150))
-    (translate (100 100 100) (sphere 80))
-    (translate (-10 -10 -100) (sphere 100))
-    (translate (100 0 55) (rotate-y 45 (box 60 400 60)))
-    (translate (-100 -100 0) (rotate-z 45 (box 50 50 400)))))
+  '(union
+    (minus
+     (sphere 200)
+     (translate (-50 -50 100) (sphere 150))
+     (translate (100 100 100) (sphere 80))
+     (translate (-10 -10 -100) (sphere 100))
+     (translate (100 0 55) (rotate-y 45 (box 60 400 60)))
+     (translate (-100 -100 0) (rotate-z 45 (box 50 50 400))))
+
+    (minus
+     (inter
+      (translate (310 125 0) (sphere 50))
+      (translate (290 125 0) (sphere 50)))
+     (translate (300 160 10) (sphere 35))
+     (translate (300 90 15) (sphere 28))
+     (translate (300 128 50) (sphere 6)))
+    (translate (300 160 10) (sphere 20))
+    
+    (minus
+     (inter
+      (translate (300 0 0) (box 100 100 60))
+      (translate (300 0 0) (sphere 50)))
+     (translate (300 0 0) (sphere 40)))
+    (translate (300 20 0) (sphere 20))
+    
+    (minus
+     (translate (300 -125 0) (sphere 50))
+     (minus
+      (translate (300 -125 45) (sphere 40))
+      (translate (300 -125 20) (sphere 30))))))
+
 
 (define (munge-sphere sphere)
   (let* ([radius (cadar sphere)]
--- shaders/generated.glsl
+++ shaders/generated.glsl
@@ -2,7 +2,12 @@
 
 float GeneratedSDF(vec3 Test)
 {
-	return Minus(
+	return Union(
+		Union(
+		Union(
+		Union(
+		Union(
+		Minus(
 		Minus(
 		Minus(
 		Minus(
@@ -12,5 +17,26 @@ float GeneratedSDF(vec3 Test)
 		SphereSDF(80, Translate(vec3(100, 100, 100), Test))), 
 		SphereSDF(100, Translate(vec3(-10, -10, -100), Test))), 
 		BoxSDF(vec3(60, 400, 60), RotateY(0.7853981633974483, Translate(vec3(100, 0, 55), Test)))), 
-		BoxSDF(vec3(50, 50, 400), RotateZ(0.7853981633974483, Translate(vec3(-100, -100, 0), Test))));
+		BoxSDF(vec3(50, 50, 400), RotateZ(0.7853981633974483, Translate(vec3(-100, -100, 0), Test)))), 
+		Minus(
+		Minus(
+		Minus(
+		Inter(
+		SphereSDF(50, Translate(vec3(310, 125, 0), Test)), 
+		SphereSDF(50, Translate(vec3(290, 125, 0), Test))), 
+		SphereSDF(35, Translate(vec3(300, 160, 10), Test))), 
+		SphereSDF(28, Translate(vec3(300, 90, 15), Test))), 
+		SphereSDF(6, Translate(vec3(300, 128, 50), Test)))), 
+		SphereSDF(20, Translate(vec3(300, 160, 10), Test))), 
+		Minus(
+		Inter(
+		BoxSDF(vec3(100, 100, 60), Translate(vec3(300, 0, 0), Test)), 
+		SphereSDF(50, Translate(vec3(300, 0, 0), Test))), 
+		SphereSDF(40, Translate(vec3(300, 0, 0), Test)))), 
+		SphereSDF(20, Translate(vec3(300, 20, 0), Test))), 
+		Minus(
+		SphereSDF(50, Translate(vec3(300, -125, 0), Test)), 
+		Minus(
+		SphereSDF(40, Translate(vec3(300, -125, 45), Test)), 
+		SphereSDF(30, Translate(vec3(300, -125, 20), Test)))));
 }
